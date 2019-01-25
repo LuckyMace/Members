f79c0ecdca2464201468004b4ee7caf3ac3f90bc
LuckyMace <mace2windu@gmail.com>
2019-01-25 16:01:02 +0200

    -Add default mongoDB parameters.

--- src/main/java/com/members/list/config/CustomMongoConfig.java
+++ src/main/java/com/members/list/config/CustomMongoConfig.java
@@ -2,11 +2,18 @@ package com.members.list.config;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 
 @Configuration
 public class CustomMongoConfig extends AbstractMongoConfiguration {
+
+    @Value("${default.mongo.uri}")
+    private String defaultMongoUri;
+
+    @Value("${default.mongo.dbname}")
+    private String defaultMongoDbname;
 
     @Override
     public MongoClient mongoClient() {
@@ -14,7 +21,7 @@ public class CustomMongoConfig extends AbstractMongoConfiguration {
             String uri = System.getenv("mongo_uri").substring(0, System.getenv("mongo_uri").lastIndexOf("/"));
             return new MongoClient(new MongoClientURI(uri));
         } else {
-            return new MongoClient("mongodb://localhost:27017");
+            return new MongoClient(defaultMongoUri);
         }
 
     }
@@ -25,7 +32,7 @@ public class CustomMongoConfig extends AbstractMongoConfiguration {
             String databaseName = System.getenv("mongo_uri").substring(System.getenv("mongo_uri").lastIndexOf("/") + 1);
             return databaseName;
         } else {
-            return "TestDB";
+            return defaultMongoDbname;
         }
     }
 }
